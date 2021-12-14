d63c71b2f1c1f85b20d81df564a0138f74bcad25
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-12-14 23:04:28 +0900

    initial commit

---
+++ src/main/java/com/tamsadan/coryn/CorynApplication.java
@@ -0,0 +1,13 @@
+package com.tamsadan.coryn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamsadan/coryn/CorynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamsadan.coryn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
