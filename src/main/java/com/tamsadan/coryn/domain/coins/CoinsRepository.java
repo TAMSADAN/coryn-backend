af9e1ef34b03e4e868c96c87d5268dda9b7b37e4
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-12-17 18:06:37 +0900

    connected to aws rds mysql

--- src/main/java/com/tamsadan/coryn/domain/coins/CoinsRepository.java
+++ src/main/java/com/tamsadan/coryn/domain/coins/CoinsRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface CoinsRepository extends JpaRepository<Coins, Long> {
 
-    @Query("SELECT c FROM c ORDER BY c.id DESC")
+    @Query("SELECT c FROM Coins c ORDER BY c.id DESC")
     List<Coins> findAllDesc();
 }
